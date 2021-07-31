d71aa1e833d80762a8b782d483f5b3f5ef9df815
reda-coder <redaelouahabi98@gmail.com>
2021-07-31 15:35:28 +0100

    finition 2

--- index.php
+++ index.php
@@ -7,14 +7,32 @@
   <title>Login</title>
   <link rel="stylesheet" href="./style.css">
   <script defer src="script.js"></script>
+
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 </head>
 
 <body>
-<main id="main-holder">
-    <img width="150px" src="./images/Logo_university.png" alt="Logo" />
+  
+ 
+  
+  
+  <main id="main-holder">
+    <div class="HeadLogo" >
+      <a href="./register.html"> 
+         <i class="fas fa-sign-in-alt signIN"></i>
+      </a>
+    <img class="logo" width="150px" src="./images/Logo_university.png" alt="Logo"  />
+    </div>
+
     <h1 class="title" id="login-header">Login</h1>
     <div style="position: relative; top: -60px;">
-      <div id="login-error-msg-holder">
+      <div id="login-error-msg-holder" 
+      style="position: relative;
+      left: 200px;
+      top: 40px;
+      width: 380px;
+      
+      ">
         <p id="login-error-msg">Invalid username <span id="error-msg-second-line">and/or password</span></p>
       </div>
       
